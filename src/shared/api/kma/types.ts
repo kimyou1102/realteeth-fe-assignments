7dfe7978bbd06fe2e5cb857cb46105427bb00ba4
kimyou1102 <yoosk5485@naver.com>
2026-01-28 01:15:03 +0900

    feat: getUltraSrtNcst API 함수 생성

---
+++ src/shared/api/kma/types.ts
@@ -0,0 +1,14 @@
+export type KmaItem = {
+  baseDate: string;
+  baseTime: string;
+  category: string;
+
+  obsrValue?: string;
+
+  fcstDate?: string;
+  fcstTime?: string;
+  fcstValue?: string;
+
+  nx?: number;
+  ny?: number;
+};

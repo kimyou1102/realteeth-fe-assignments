f2105bfc41e23ce16fa26bd47e24fcee067a0e4f
kimyou1102 <yoosk5485@naver.com>
2026-01-28 04:37:42 +0900

    feat: 현재 위치 조회를 위한 geolocation 훅 적용 및 조건부 렌더링 처리

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,28 +1,38 @@
 import { HomeHeader } from "../../widgets";
 import { CurrentWeatherCard } from "../../widgets/current-weather-card/CurrentWeatherCard";
 import { useWeatherSummaryQuery } from "../../entities/weather/api/useWeatherSummaryQuery";
+import { useGeolocation } from "../../shared/lib/geolocation/useGeolocation";
+import { LocationPermissionDenied } from "../../shared/ui/location-permission-denied/LocationPermissionDenied";
 
 export function HomePage() {
-  const lat = 37.49265;
-  const lon = 126.8895972;
+  const { coords, isLoading, error } = useGeolocation();
 
-  const { data } = useWeatherSummaryQuery({ lat, lon });
+  const { data } = useWeatherSummaryQuery({
+    lat: coords?.lat ?? 0,
+    lon: coords?.lng ?? 0,
+  });
+
+  if (!coords || isLoading) return <div>위치 확인 중...</div>;
 
   return (
     <main>
       <HomeHeader />
-      <CurrentWeatherCard
-        locationName="Busan"
-        temperature={data?.current.tempC ?? 0}
-        conditionLabel={data?.current.conditionText ?? null}
-        tempMax={data?.today.maxTempC ?? 0}
-        tempMin={data?.today.minTempC ?? 0}
-        humidity={data?.current.humidityPct ?? 0}
-        windSpeed={data?.current.windSpeedMs ?? 0}
-        // TODO: 즐겨찾기 기능 추가 예정
-        isFavorite
-        onFavoriteToggle={() => {}}
-      />
+      {error ? (
+        <LocationPermissionDenied />
+      ) : (
+        <CurrentWeatherCard
+          locationName="Busan"
+          temperature={data?.current.tempC ?? 0}
+          conditionLabel={data?.current.conditionText ?? null}
+          tempMax={data?.today.maxTempC ?? 0}
+          tempMin={data?.today.minTempC ?? 0}
+          humidity={data?.current.humidityPct ?? 0}
+          windSpeed={data?.current.windSpeedMs ?? 0}
+          // TODO: 즐겨찾기 기능 추가 예정
+          isFavorite
+          onFavoriteToggle={() => {}}
+        />
+      )}
     </main>
   );
 }

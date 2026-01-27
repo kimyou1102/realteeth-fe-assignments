28bbd5cad02b3124fc1177a25807a0ba51e5737f
kimyou1102 <yoosk5485@naver.com>
2026-01-28 01:35:25 +0900

    feat: 홈페이지에 HomeHeader, CurrentWeatherCard 컴포넌트 추가

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,7 +1,21 @@
+import { HomeHeader } from "../../widgets";
+import { CurrentWeatherCard } from "../../widgets/current-weather-card/CurrentWeatherCard";
+
 export function HomePage() {
   return (
     <main>
-      <h1>Home</h1>
+      <HomeHeader />
+      <CurrentWeatherCard
+        locationName="Busan"
+        temperature={0}
+        conditionLabel="맑음"
+        tempMax={0}
+        tempMin={0}
+        humidity={25}
+        windSpeed={2.57}
+        isFavorite
+        onFavoriteToggle={() => {}}
+      />
     </main>
   );
 }

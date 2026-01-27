771d79a1881d0487a87881f13db225108bdf5794
kimyou1102 <yoosk5485@naver.com>
2026-01-28 03:19:42 +0900

    feat: 실제 날씨 API 연동해서 실제 데이터로 렌더

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,18 +1,25 @@
 import { HomeHeader } from "../../widgets";
 import { CurrentWeatherCard } from "../../widgets/current-weather-card/CurrentWeatherCard";
+import { useWeatherSummaryQuery } from "../../entities/weather/api/useWeatherSummaryQuery";
 
 export function HomePage() {
+  const lat = 37.49265;
+  const lon = 126.8895972;
+
+  const { data } = useWeatherSummaryQuery({ lat, lon });
+
   return (
     <main>
       <HomeHeader />
       <CurrentWeatherCard
         locationName="Busan"
-        temperature={0}
-        conditionLabel="맑음"
-        tempMax={0}
-        tempMin={0}
-        humidity={25}
-        windSpeed={2.57}
+        temperature={data?.current.tempC ?? 0}
+        conditionLabel={data?.current.conditionText ?? null}
+        tempMax={data?.today.maxTempC ?? 0}
+        tempMin={data?.today.minTempC ?? 0}
+        humidity={data?.current.humidityPct ?? 0}
+        windSpeed={data?.current.windSpeedMs ?? 0}
+        // TODO: 즐겨찾기 기능 추가 예정
         isFavorite
         onFavoriteToggle={() => {}}
       />

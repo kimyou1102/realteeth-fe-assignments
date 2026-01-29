73a0153b0c5ddc5df8f499ce4a127b07c0d1c361
kimyou1102 <yoosk5485@naver.com>
2026-01-30 03:03:13 +0900

    feat: 홈 화면에 시간별 예보 컴포넌트 추가

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -5,6 +5,7 @@ import { useGeolocation } from "../../shared/lib/geolocation/useGeolocation";
 import { LocationPermissionDenied } from "../../shared/ui/location-permission-denied/LocationPermissionDenied";
 import { useQuery } from "@tanstack/react-query";
 import { reverseGeocodeKoreanAdmin } from "../../shared/lib/kakao/reverseGeocodeKoreanAdmin";
+import { HourlyForecast } from "../../widgets/hourly-forecast/HourlyForecast";
 
 export function HomePage() {
   const { coords, isLoading, error } = useGeolocation();
@@ -13,7 +14,6 @@ export function HomePage() {
     lat: coords?.lat,
     lon: coords?.lng,
   });
-
   const { data: adminRegion } = useQuery({
     queryKey: ["adminRegion", coords?.lat, coords?.lng],
     enabled: coords?.lat != undefined && coords?.lng != undefined,
@@ -33,18 +33,21 @@ export function HomePage() {
       {error ? (
         <LocationPermissionDenied />
       ) : (
-        <CurrentWeatherCard
-          locationName={adminRegion ?? ""}
-          temperature={data?.current.tempC ?? 0}
-          conditionLabel={data?.current.conditionText ?? null}
-          tempMax={data?.today.maxTempC ?? 0}
-          tempMin={data?.today.minTempC ?? 0}
-          humidity={data?.current.humidityPct ?? 0}
-          windSpeed={data?.current.windSpeedMs ?? 0}
-          // TODO: 즐겨찾기 기능 추가 예정
-          isFavorite
-          onFavoriteToggle={() => {}}
-        />
+        <>
+          <CurrentWeatherCard
+            locationName={adminRegion ?? ""}
+            temperature={data?.current.tempC ?? 0}
+            conditionLabel={data?.current.conditionText ?? null}
+            tempMax={data?.today.maxTempC ?? 0}
+            tempMin={data?.today.minTempC ?? 0}
+            humidity={data?.current.humidityPct ?? 0}
+            windSpeed={data?.current.windSpeedMs ?? 0}
+            // TODO: 즐겨찾기 기능 추가 예정
+            isFavorite
+            onFavoriteToggle={() => {}}
+          />
+          <HourlyForecast forecasts={data?.hourly3h ?? []} />
+        </>
       )}
     </main>
   );
